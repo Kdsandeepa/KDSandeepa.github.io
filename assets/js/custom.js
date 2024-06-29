f52b9b09162ab513cf70e566f9878b25a780f187
Kavishka Dinith Sandeepa Sandeepa <kdsandeepaahm@gmail.com>
2024-06-29 13:20:31 +0530

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.querySelector('a.button.icon.solid.solo.fa-arrow-down.scrolly');
+    button.addEventListener('click', function() {
+        alert('Button clicked!');
+    });
+});
